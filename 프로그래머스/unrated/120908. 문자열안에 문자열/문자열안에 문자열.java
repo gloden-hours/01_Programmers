b1da4f86759c36cb25471e9ba5ad7da2386b1862
한솔 <149558427+gloden-hours@users.noreply.github.com>
2023-12-27 16:07:40 +0900

    [level unrated] Title: 문자열안에 문자열, Time: 0.04 ms, Memory: 71 MB -BaekjoonHub

--- 프로그래머스/unrated/120908. 문자열안에 문자열/문자열안에 문자열.java
+++ 프로그래머스/unrated/120908. 문자열안에 문자열/문자열안에 문자열.java
@@ -1,7 +1,5 @@
 class Solution {
     public int solution(String str1, String str2) {
-        str1 = str1.replace(str2, " ");
-        
-        return str1.contains(" ") ? 1 : 2;
+        return str1.contains(str2) ? 1 : 2;
     }
 }
