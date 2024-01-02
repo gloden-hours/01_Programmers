644f75193ec04be79a1da01e97b19c450412d63f
한솔 <149558427+gloden-hours@users.noreply.github.com>
2024-01-02 18:29:22 +0900

    [level unrated] Title: 숨어있는 숫자의 덧셈 (1), Time: 6.46 ms, Memory: 72.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(String my_string) {
+        int answer = 0;
+        
+        for (int i = 0; i < my_string.length(); i++) {
+            if(48 <= my_string.charAt(i) && my_string.charAt(i) <= 57) {
+                answer += Integer.parseInt(my_string.charAt(i)+"");
+            }
+        }
+        
+        return answer;
+    }
+}
