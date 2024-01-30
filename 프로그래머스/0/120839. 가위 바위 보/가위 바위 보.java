b86ebbbb25cdc2dea0fad0c3bddbec01360f6608
한솔 <149558427+gloden-hours@users.noreply.github.com>
2024-01-30 17:03:59 +0900

    [level 0] Title: 가위 바위 보, Time: 4.35 ms, Memory: 82.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String solution(String rsp) {
+        String answer = "";
+        
+        char[] rsp_array = rsp.toCharArray();
+
+        for (int i = 0; i < rsp_array.length; i++) {
+            if( rsp_array[i] == '2' ) answer += 0;
+            else if( rsp_array[i] == '0' ) answer += 5;
+            else answer += 2;
+        }
+        
+        return answer;
+    }
+}
